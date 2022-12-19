4b1c3d7ed5a0853ed19ae7b92c4d6b7c0c42404b
deea87 <113915219+deea87@users.noreply.github.com>
2022-12-19 13:45:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('its working');
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]',
+);
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  if (currentTheme === 'light') {
+    document.querySelector('#theme').href = '';
+  }
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    document.querySelector('#theme').href = 'darkmode.css';
+  }
+}
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.querySelector('#theme').href = 'darkmode.css';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.querySelector('#theme').href = '';
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
